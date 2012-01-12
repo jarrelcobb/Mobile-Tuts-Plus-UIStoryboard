be97d671400e05c9972b7d2db2900153f7312234
Jarrel Cobb <jarrelc@gmail.com>
2012-01-12 10:35:01 -0500

    added NewTable

---
+++ MobileTutsStoryboarding/NewTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewTableViewController.h
+//  MobileTutsStoryboarding
+//
+//  Created by Jarrel Cobb on 1/12/12.
+//  Copyright (c) 2012 ELC Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewTableViewController : UITableViewController
+
+@end
